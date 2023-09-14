b8e70e0d357834e30f3fee63d519628bad08b81e
enson <enson.varghese@cubettech.com>
2023-09-14 13:00:33 +0530

    jwt implimentation

--- database.py
+++ database.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-URL_DATABASE =os.getenv('URL_DATABASE')
-
+# URL_DATABASE =os.getenv('URL_DATABASE')
+URL_DATABASE = 'mysql+pymysql://root:root@172.17.0.1:3307/mysql'
 engine = create_engine(URL_DATABASE)
 
 sessionLocal = sessionmaker(autoflush=False, bind=engine, autocommit=False)
--- main.py
+++ main.py
@@ -4,8 +4,20 @@ from typing import Annotated
 import models
 from database import engine, sessionLocal
 from sqlalchemy.orm import Session
-# import schemas
-from typing import List
+from dep import get_current_user
+from utils import get_hashed_password
+from uuid import uuid4
+from fastapi import FastAPI, status, HTTPException, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.responses import RedirectResponse
+from schemas import UserOut, UserAuth, TokenSchema
+from utils import (
+    get_hashed_password,
+    create_access_token,
+    create_refresh_token,
+    verify_password
+)
+from uuid import uuid4
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -16,7 +28,7 @@ class PostBase(BaseModel):
     user_id:int
    
 class UserBase(BaseModel):
-    username: str
+    email: str
 
 def get_db():
     db = sessionLocal()
@@ -27,44 +39,58 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-# @app.post("/register")
-# def register_user(user: schemas.UserCreate, session: Session = Depends(get_session)):
-#     existing_user = session.query(models.User).filter_by(email=user.email).first()
-#     if existing_user:
-#         raise HTTPException(status_code=400, detail="Email already registered")
+@app.post('/signup', summary="Create new user", response_model=UserOut)
+async def create_user(data: UserAuth, db: db_dependency):
+    # querying database to check if user already exist
+    user = db.get(data.email, None)
+    if user is not None:
+            raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User with this email already exist"
+        )
+    user = {
+        'email': data.email,
+        'password': get_hashed_password(data.password),
+        'id': str(uuid4())
+    }
+    db[data.email] = user    # saving user to database
+    return user
 
-#     encrypted_password =get_hashed_password(user.password)
 
-#     new_user = models.User(username=user.username, email=user.email, password=encrypted_password )
+@app.post('/login', summary="Create access and refresh tokens for user", response_model=TokenSchema)
+async def login(db:db_dependency, form_data: OAuth2PasswordRequestForm = Depends()):
+    user = db.get(form_data.username, None)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect email or password"
+        )
 
-#     session.add(new_user)
-#     session.commit()
-#     session.refresh(new_user)
+    hashed_pass = user['password']
+    if not verify_password(form_data.password, hashed_pass):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect email or password"
+        )
+    
+    return {
+        "access_token": create_access_token(user['email']),
+        "refresh_token": create_refresh_token(user['email']),
+    }
 
-#     return {"message":"user created successfully"}
+@app.get('/me', summary='Get details of currently logged in user', response_model=UserOut)
+async def get_me(user: UserBase = Depends(get_current_user)):
+    return user
 
 @app.post("/posts/", status_code=status.HTTP_201_CREATED)
-async def create_post(post: PostBase, db: db_dependency):
+async def create_post(post: PostBase, db: db_dependency, user: UserBase = Depends(get_current_user)):
     db_post = models.Post(**post.dict())
     db.add(db_post)
     db.commit()
 
 @app.get("/posts/{posts_id}", status_code=status.HTTP_200_OK)
-async def get_post(posts_id: int, db: db_dependency):
-    user = db.query(models.Post).filter(models.Post.id == posts_id).first()
-    if user is None:
+async def get_post(posts_id: int, db: db_dependency, user: UserBase = Depends(get_current_user)):
+    post = db.query(models.Post).filter(models.Post.id == posts_id).first()
+    if post is None:
         raise Exception(status.HTTP_404_NOT_FOUND, detail='post not found')
-    return user
-
-@app.post("/users/", status_code=status.HTTP_201_CREATED)
-async def create_user(user: UserBase, db: db_dependency):
-    db_user = models.User(**user.dict())
-    db.add(db_user)
-    db.commit()
-
-@app.get("/users/{user_id}", status_code=status.HTTP_200_OK)
-async def get_user(user_id: int, db: db_dependency):
-    user = db.query(models.User).filter(models.User.id == user_id).first()
-    if user is None:
-        raise Exception(status.HTTP_404_NOT_FOUND, detail='user not found')
-    return user
+    return post
